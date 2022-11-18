827024ef835ee8131539c23f29c17ef5a2140aaa
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-11-18 11:58:58 -0300

    Async Aula

---
+++ lib/4_async/4_futures_foreach/1_futures_foreach.dart
@@ -0,0 +1,25 @@
+import '../../3_dart_poo/14_assignment_operators/1_assignment_operators.dart';
+
+Future<void> main() async {
+  var nomes = ['Carlos', 'Cristiane', 'Sonia'];
+  print('Inicio');
+
+  // Fazer esse tipo de iteração não funciona com foreach.
+  // nomes.forEach((nome) async {
+  //   print(await saudacao(nome));
+  // });
+
+  // utilizando future.foreach
+  await Future.forEach<String>(nomes, (nome) async {
+    print(await saudacao(nome));
+  });
+  // Utilizando FOR
+  for (var nome in nomes) {
+    print(await saudacao(nome));
+  }
+  print('Fim');
+}
+
+Future<String> saudacao(String nome) {
+  return Future.delayed(Duration(seconds: 1), () => 'Olá $nome');
+}
