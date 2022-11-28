df12dc925e359fa9eeca9117ff4d8ac0f6f51414
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-11-28 17:06:18 -0300

    Atualização

---
+++ lib/5_dart_banco_dados/dart_banco_dados.dart
@@ -0,0 +1,55 @@
+import 'package:dart_fundamentos/5_dart_banco_dados/database.dart';
+import 'package:mysql1/mysql1.dart';
+
+Future<void> main() async {
+  final database = Database();
+  var mySqlConnection = await database.openConnection();
+
+  // Insert
+  /*await mySqlConnection.query(
+     'insert into aluno values(?,?)',
+     [null, 'Carlos Alberto'],
+  );
+
+  await mySqlConnection.query(
+     'insert into aluno values(?,?)',
+     [null, 'Cristiane Cunha'],
+  );
+
+  await mySqlConnection.query(
+     'insert into aluno values(?,?)',
+     [null, 'Extra Carlos'],
+  );*/
+
+  // Update
+
+  try {
+    await mySqlConnection.query(
+      "update aluno set nome =? where id = ?",
+      [4, 'Carlos Neto'],
+    );
+  } on MySqlException catch (e) {
+    print('Erro ao realizar a atualziação do dado, $e');
+  }
+
+  //await mySqlConnection.transaction((conn) {});
+
+  // Consulta todos resultado.
+  // final retornoSelect = await mySqlConnection.query('Select * from aluno');
+  // retornoSelect.forEach((row) {
+  //   print("ID: ${row['id']}");
+  //   print("Nome: ${row['nome']}");
+  // });
+
+  // Consulta resultado unico
+  final retornoSelectUnico = await mySqlConnection.query(
+    'select * from aluno where id = ?',
+    [1],
+  );
+
+  if (retornoSelectUnico.isNotEmpty) {
+    var rowUnico = retornoSelectUnico.first;
+    print("ID: ${rowUnico['id']}");
+    print("Nome: ${rowUnico['nome']}");
+  }
+}
---
+++ lib/5_dart_banco_dados/database.dart
@@ -0,0 +1,17 @@
+import 'package:mysql1/mysql1.dart';
+
+class Database {
+  Future<MySqlConnection> openConnection() async {
+    final settings = ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      //password: 'caneto@rj',
+      db: 'dart_mysql',
+    );
+
+    final connection = await MySqlConnection.connect(settings);
+    await Future.delayed(Duration(seconds: 1));
+    return connection;
+  }
+}
