09af5c4f02a146fdd4da78c8240ab3db069bd57e
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-15 12:09:40 -0300

    Atualização

--- alunos_cli/lib/src/commands/students/subcommands/insert_command.dart
+++ alunos_cli/lib/src/commands/students/subcommands/insert_command.dart
@@ -1,8 +1,10 @@
-
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
 
+import '../../../models/address.dart';
+import '../../../models/city.dart';
+import '../../../models/phone.dart';
 import '../../../models/student.dart';
 import '../../../respositories/product_repository.dart';
 import '../../../respositories/student_repository.dart';
@@ -17,12 +19,13 @@ class InsertCommand extends Command {
   @override
   String get name => 'insert';
 
-  InsertCommand(this.studentRepository) : productRepository = ProductRepository() {
+  InsertCommand(this.studentRepository)
+      : productRepository = ProductRepository() {
     argParser.addOption('file', help: 'Path of the csv file', abbr: 'f');
   }
-  
+
   @override
-  void run() {
+  Future<void> run() async {
     print('Aguarde ...');
     final filePath = argResults?['file'];
     final students = File(filePath).readAsLinesSync();
@@ -31,15 +34,37 @@ class InsertCommand extends Command {
     for (var student in students) {
       final studentData = student.split(';');
       final courseCsv = studentData[2].split(',').map((e) => e.trim()).toList();
-      
-      courseCsv.map((c) async {
+
+      final courseFutures = courseCsv.map((c) async {
         final course = await productRepository.findByName(c);
         course.isStudent = true;
         return course;
-      });
-      
+      }).toList();
+
+      final courses = await Future.wait(courseFutures);
+
+      final studentModel = Student(
+        name: studentData[0],
+        age: int.tryParse(studentData[1]),
+        nameCourses: courseCsv,
+        courses: courses,
+        address: Address(
+          street: studentData[3],
+          number: int.parse(studentData[4]),
+          zipCode: studentData[5],
+          city: City(id: 1, name: studentData[6]),
+          phone: Phone(
+            ddd: int.parse(studentData[7]),
+            phone: studentData[8],
+          ),
+        ),
+      );
+
+      await studentRepository.insert(studentModel);
+    
     }
 
+    print('............................................');
+    print('Alunos adicionaos com sucesso');
   }
-
 }
