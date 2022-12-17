005b11b86d00003bd3454cd03ddd7ac85018c638
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-17 00:10:32 -0300

    Atualização Update

--- alunos_cli/lib/src/commands/students/subcommands/update_command.dart
+++ alunos_cli/lib/src/commands/students/subcommands/update_command.dart
@@ -1,6 +1,12 @@
 
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
 
+import '../../../models/address.dart';
+import '../../../models/city.dart';
+import '../../../models/phone.dart';
+import '../../../models/student.dart';
 import '../../../respositories/product_repository.dart';
 import '../../../respositories/student_repository.dart';
 
@@ -20,7 +26,54 @@ class UpdateCommand extends Command {
   }
   
   @override
-  void run() {
+  Future<void> run() async {
+    print('Aguarde ...');
+    final filePath = argResults?['file'];
+    final id = argResults?['id'];
 
+    if(id == null) {
+      print('Por favor informe o id do aluno com o comando --id=0 ou -i 0');
+      return;
+    }
+
+    final students = File(filePath).readAsLinesSync();
+    print('Aguarde, atualizando dados do Aluno');
+    print('............................................');
+
+    for (var student in students) {
+      final studentData = student.split(';');
+      final courseCsv = studentData[2].split(',').map((e) => e.trim()).toList();
+
+      final courseFutures = courseCsv.map((c) async {
+        final course = await productRepository.findByName(c);
+        course.isStudent = true;
+        return course;
+      }).toList();
+
+      final courses = await Future.wait(courseFutures);
+
+      final studentModel = Student(
+        name: studentData[0],
+        age: int.tryParse(studentData[1]),
+        nameCourses: courseCsv,
+        courses: courses,
+        address: Address(
+          street: studentData[3],
+          number: int.parse(studentData[4]),
+          zipCode: studentData[5],
+          city: City(id: 1, name: studentData[6]),
+          phone: Phone(
+            ddd: int.parse(studentData[7]),
+            phone: studentData[8],
+          ),
+        ),
+      );
+
+      await studentRepository.insert(studentModel);
+    
+    }
+
+    print('............................................');
+    print('Aluno atualizado com sucesso');
   }
 }
