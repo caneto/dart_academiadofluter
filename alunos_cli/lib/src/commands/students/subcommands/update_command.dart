6605c7a169283f553337ea0ab01286624cfd6ab1
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-17 12:25:49 -0300

    Fim do Update

--- alunos_cli/lib/src/commands/students/subcommands/update_command.dart
+++ alunos_cli/lib/src/commands/students/subcommands/update_command.dart
@@ -40,38 +40,46 @@ class UpdateCommand extends Command {
     print('Aguarde, atualizando dados do Aluno');
     print('............................................');
 
-    for (var student in students) {
-      final studentData = student.split(';');
-      final courseCsv = studentData[2].split(',').map((e) => e.trim()).toList();
-
-      final courseFutures = courseCsv.map((c) async {
-        final course = await productRepository.findByName(c);
-        course.isStudent = true;
-        return course;
-      }).toList();
-
-      final courses = await Future.wait(courseFutures);
-
-      final studentModel = Student(
-        name: studentData[0],
-        age: int.tryParse(studentData[1]),
-        nameCourses: courseCsv,
-        courses: courses,
-        address: Address(
-          street: studentData[3],
-          number: int.parse(studentData[4]),
-          zipCode: studentData[5],
-          city: City(id: 1, name: studentData[6]),
-          phone: Phone(
-            ddd: int.parse(studentData[7]),
-            phone: studentData[8],
-          ),
-        ),
-      );
-
-      await studentRepository.insert(studentModel);
-    
+    if(students.length > 1) {
+      print('Por favor informe somente um aluno no arquivo $filePath');
+      return;
+    } else if (students.isEmpty) {
+      print('Por favor informe um aluno no arquivo $filePath');
+      return;
     }
+
+    var student = students.first;
+
+    final studentData = student.split(';');
+    final courseCsv = studentData[2].split(',').map((e) => e.trim()).toList();
+
+    final courseFutures = courseCsv.map((c) async {
+      final course = await productRepository.findByName(c);
+      course.isStudent = true;
+      return course;
+    }).toList();
+
+    final courses = await Future.wait(courseFutures);
+    
+    final studentModel = Student(
+      id: int.parse(id),
+      name: studentData[0],
+      age: int.tryParse(studentData[1]),
+      nameCourses: courseCsv,
+      courses: courses,
+      address: Address(
+        street: studentData[3],
+        number: int.parse(studentData[4]),
+        zipCode: studentData[5],
+        city: City(id: 1, name: studentData[6]),
+        phone: Phone(
+          ddd: int.parse(studentData[7]),
+          phone: studentData[8],
+        ),
+      ),
+    );
+
+    await studentRepository.update(studentModel);
 
     print('............................................');
     print('Aluno atualizado com sucesso');
